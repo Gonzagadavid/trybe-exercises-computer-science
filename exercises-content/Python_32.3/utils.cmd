90d5e0b0b0c46059eafcc33c637a46f11693bcb2
Gonzagadavid <gonzagadaviddev@gmail.com>
2022-02-10 22:47:59 -0300

    adicionado conteudo do dia 32.3

---
+++ exercises-content/Python_32.3/utils.cmd
@@ -0,0 +1,11 @@
+@REM A instalação é feita através do pip utilizando o comando:
+python3 -m pip install pytest
+
+@REM E podemos verificar utilizando o comando:
+python3 -m pytest --version
+
+@REM A saída esperada é similar à apresentada abaixo.
+This is pytest version 5.3.0, imported from /home/cassiobotaro/projects/gerenciador-tarefas/.venv/lib/python3.8/site-packages/pytest.py
+
+@REM Rodar os testes
+python3 -m pytest
