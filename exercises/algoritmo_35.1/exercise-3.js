46786bdc7c3d78dff7767a9d4b3f9807425ca4c9
gonzagadavid <david.blesses@hotmail.com>
2022-03-16 14:34:34 -0300

    adicionado exercicios do dias 35.1

---
+++ exercises/algoritmo_35.1/exercise-3.js
@@ -0,0 +1,6 @@
+// Exercício 3 O código abaixo está em JavaScript . Calcule sua ordem de complexidade para um array de tamanho n .
+
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+numbers.map(n => n*n)
+
+// A função map itera sobre todo o array. O código, portanto, é O(n).
