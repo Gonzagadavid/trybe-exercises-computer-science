85f2a4f438a73a428af0027a528f35821f506f39
gonzagadavid <david.blesses@hotmail.com>
2022-04-04 19:03:12 -0300

    feat: adicionado conteudo do dia 37.2

---
+++ exercises-content/pilhas_37.2/process_video.py
@@ -0,0 +1,29 @@
+def load_video(video_path):
+    print('Carregando vídeo do caminho:', video_path)
+    return 'fake vídeo'
+
+def process_video(video_path):
+    print('Carregando vídeo...')
+    loaded_video = load_video(video_path)
+    # Faz alguma coisa legal com o vídeo
+
+
+
+# codigo abaixo para IDLE do python
+
+# import traceback
+# import sys
+
+
+# def load_video(video_path):
+#     print('Carregando vídeo do caminho:', video_path)
+#     traceback.print_stack(file=sys.stdout)
+#     return 'fake vídeo'
+
+# def process_video(video_path):
+#     print('Carregando vídeo...')
+#     loaded_video = load_video(video_path)
+#     # Faz alguma coisa legal com o vídeo
+
+
+process_video('path/to/my/video')
